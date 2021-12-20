17acb2121edcba9b215cd35f22ce7c2d3c5dd9b9
shiffinjthomas <shiffin144@gmail.com>
2021-12-20 17:16:31 +0530

    ean13 bug fixed both 12 &13 digits acepted

--- lib/product.dart
+++ lib/product.dart
@@ -96,9 +96,7 @@ class _ProductState extends State<Product> {
   Future check() async {
     try {
       if (_url != null && _ref == null) {
-        if (_url.length == 8 || _url.length == 13) {
-          // print(
-          //     '$_site/products?filter[reference]=${_url.substring(0, _url.length - 1)}&display=full&output_format=JSON&$_key');
+        if (_url.length == 8) {
           var response = await http.get(Uri.parse(
               '$_site/products?filter[reference]=${_url.substring(0, _url.length - 1)}&display=full&output_format=JSON&$_key'));
           var data = jsonDecode(utf8.decode(response.bodyBytes));
@@ -108,6 +106,48 @@ class _ProductState extends State<Product> {
           var data2 = jsonDecode(utf8.decode(response2.bodyBytes));
           data['stock'] = data2['stock_availables'][0];
           // print(data);
+          if (data != null) {
+            connectionStatus = true;
+            //  print("connected $connectionStatus");
+          }
+
+          if (!_exicuted) {
+            if (data['products'][0]['active'].toString() == '1') {
+              _switchValue = true;
+            }
+          }
+
+          return data;
+        }
+        if (_url.length == 13) {
+          var response = await http.get(Uri.parse(
+              '$_site/products?filter[reference]=$_url&display=full&output_format=JSON&$_key'));
+          var data = jsonDecode(utf8.decode(response.bodyBytes));
+          if (data.isEmpty) {
+            var response = await http.get(Uri.parse(
+                '$_site/products?filter[reference]=${_url.substring(0, _url.length - 1)}&display=full&output_format=JSON&$_key'));
+            var data = jsonDecode(utf8.decode(response.bodyBytes));
+            var response2 = await http.get(Uri.parse(
+                '$_site/stock_availables?filter[id_product]=${data['products'][0]['id']}&display=full&output_format=JSON&$_key'));
+            var data2 = jsonDecode(utf8.decode(response2.bodyBytes));
+            data['stock'] = data2['stock_availables'][0];
+            if (data != null) {
+              connectionStatus = true;
+            }
+
+            if (!_exicuted) {
+              if (data['products'][0]['active'].toString() == '1') {
+                _switchValue = true;
+              }
+            }
+
+            return data;
+          }
+          var response2 = await http.get(Uri.parse(
+              '$_site/stock_availables?filter[id_product]=${data['products'][0]['id']}&display=full&output_format=JSON&$_key'));
+          var data2 = jsonDecode(utf8.decode(response2.bodyBytes));
+          data['stock'] = data2['stock_availables'][0];
+
           if (data != null) {
             connectionStatus = true;
             //  print("connected $connectionStatus");
